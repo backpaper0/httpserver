5db6ac03ffa0f6be7f6c0f8a6faef9186b59d439
Taichi Uragami <backpaper0@gmail.com>
2016-02-21 13:55:07 +0900

    サンプルのechoサーバーを雑に修正

--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -21,7 +21,13 @@ public class EchoServer {
         Map<String, List<String>> headers = new HashMap<>();
         headers.computeIfAbsent("Content-Type", key -> new ArrayList<>())
                 .add("text/plain; charset=UTF-8");
-        ByteBuffer entity = request.entity;
+        String s;
+        if (request.method.equals("GET")) {
+            s = request.requestTarget;
+        } else {
+            s = new String(request.entity.array());
+        }
+        ByteBuffer entity = ByteBuffer.wrap(s.split("=")[1].getBytes());
         return new HttpResponse(statusCode, reasonPhrase, headers, entity);
     }
 }

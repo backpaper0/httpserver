5baea04405eb4a185f1027818ea435bde4d7e6b0
Taichi Uragami <backpaper0@gmail.com>
2018-03-20 07:32:30 +0900

    とりあえずエントリーポイントを追加

---
+++ src/main/java/jp/urgm/httpserver/HttpServer.java
@@ -0,0 +1,5 @@
+package jp.urgm.httpserver;
+
+public class HttpServer {
+
+}
---
+++ src/test/java/jp/urgm/httpserver/HttpServerTest.java
@@ -0,0 +1,13 @@
+package jp.urgm.httpserver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HttpServerTest extends HttpServer {
+
+    @Test
+    void test() {
+        assertNotNull(HttpServer.class);
+    }
+}
